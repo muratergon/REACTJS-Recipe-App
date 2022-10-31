4dc50553b50f5fb1ca043562f615e91ca874970e
murat <ergon.muratt@gmail.com>
2022-10-31 21:40:26 +0300

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import LoginContainer, {
+  FormContainer,
+  Header,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import meal from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Login
+  const userInfo = {
+    username: "admin",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<CW/>"}RECIPE</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter Your Name" type="text" />
+          <StyledInput placeholder="Password" type="password" />
+          <StyledButton type="submit">LOGIN</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
