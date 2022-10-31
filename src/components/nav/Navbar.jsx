9a384747f8a32edd5f46ce6b1c015b631828920c
murat <ergon.muratt@gmail.com>
2022-10-31 22:47:20 +0300

    menu

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.style";
-import { Nav } from "./Navbar.style";
+import Nav, { Brand, Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Nav justify="space-between" wrap="wrap">
-      <a href="">
+      <Brand to="/">
         <i>{"<Clarusway/>"}</i>
-        <span>RECIPE</span>
-      </a>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/logout">Logout</Link>
-      </div>
+        <span>Recipe</span>
+      </Brand>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </Hamburger>
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="Logout">Logout</MenuLink>
+      </Menu>
     </Nav>
   );
 };
