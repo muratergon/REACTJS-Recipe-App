ed3733a8d78e389f316a2acfb5a2ec5f1849eae8
murat <ergon.muratt@gmail.com>
2022-11-01 20:32:46 +0300

    headerstart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Header.style";
+import { FormContainer, HeaderContainer, MainHeader } from "./Header.style";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <HeaderContainer>
+      <MainHeader>RECIPE APP</MainHeader>
+      <FormContainer></FormContainer>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const APP_ID = "c10b6d21";
+  const APP_KEY = "05d33571024533fee9a60b7f1474de84";
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState([]);
 
-export default Home
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    const { data } = await axios(url);
+    console.log(data);
+    setRecipes(data.hits);
+  };
+  console.log(recipes);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
